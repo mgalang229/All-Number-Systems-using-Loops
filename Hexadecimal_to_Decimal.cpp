385bf3a20aa127c4f76e379935ceec6dc49e967a
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-11-07 15:13:45 +0800

    Create Hexadecimal_to_Decimal.cpp

---
+++ Hexadecimal_to_Decimal.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	// Works ONLY for Capital Letters
+	string s;
+	cin >> s;
+	int b=1, d=0;
+	for(int i=(int)s.size()-1; i>=0; --i) {
+		if(s[i]>='0'&&s[i]<='9') {
+			d+=(s[i]-48)*b;
+			b*=16;
+		} else if(s[i]>='A'&&s[i]<='F') {
+			d+=(s[i]-55)*b;
+			b*=16;
+		}
+	}
+	cout << d << "\n";
+}
