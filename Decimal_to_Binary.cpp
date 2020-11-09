756e020943aa4b93ad5e6f05a13f0d668464f308
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-11-09 20:17:53 +0800

    Update Decimal_to_Binary.cpp

--- Decimal_to_Binary.cpp
+++ Decimal_to_Binary.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
+#define ll long long
+#define ar array
+
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
+	
 	int n;
 	cin >> n;
-	int a[1000];
-	int cnt = 0;
-	while (n > 0) {
-		a[cnt++] = n % 2;
-		n /= 2;
+	int a[1000], cnt=0;
+	while(n>0) {
+		a[cnt++]=n%2;
+		n/=2;
 	}
-	for (int i = cnt - 1; i >= 0; i--) {
+	for(int i=cnt-1; i>=0; --i)
 		cout << a[i];
-	}
-	cout << '\n';
-	return 0;
+	cout << "\n";
 }
