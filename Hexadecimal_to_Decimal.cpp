c377ec499562c4f89b386c85ec20f94af52b239f
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-11-09 20:23:25 +0800

    Update Hexadecimal_to_Decimal.cpp

--- Hexadecimal_to_Decimal.cpp
+++ Hexadecimal_to_Decimal.cpp
@@ -1,36 +1,35 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int Power(int a, int b) {
-	int res = 1;
-	while (b > 0) {
-		if (b & 1) {
-			res *= a;
-		}
-		a *= a;
-		b /= 2;
+#define ll long long
+#define ar array
+
+int power(int a, int b) {
+	int res=1;
+	while(b>0) {
+		if(b&1)
+			res*=a;
+		a*=a;
+		b/=2;
 	}
 	return res;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
+	
 	string s;
 	cin >> s;
-	int b = 1;
-	int d = 0;
-	for (int i = (int) s.size() - 1; i >= 0; i--) {
-		if (s[i] >= '0' && s[i] <= '9') {
-			d += (s[i] - 48) * b;
-		} else if (s[i] >= 'A' && s[i] <= 'F') {
-			d += (s[i] - 55) * b;
-		} else if (s[i] >= 'a' && s[i] <= 'f') {
-			d += (s[i] - 87) * b;
-		}
-		b *= 16;
+	int b=1, d=0;
+	for(int i=(int)s.size()-1; i>=0; --i) {
+		if(s[i]>='0'&&s[i]<='9')
+			d+=(s[i]-48)*b;
+		else if(s[i]>='A'&&s[i]<='F')
+			d+=(s[i]-55)*b;
+		else if(s[i]>='a'&&s[i]<='f')
+			d+=(s[i]-87)*b;
+		b*=16;
 	}
-	cout << d << '\n';
-	return 0;
+	cout << d << "\n";
 }
