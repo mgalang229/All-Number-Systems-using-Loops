1812fc8cd05669f33e20c94c080b4bd5ff459fe2
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-11-09 14:51:43 +0800

    Update Octal_to_Hexadecimal.cpp

--- Octal_to_Hexadecimal.cpp
+++ Octal_to_Hexadecimal.cpp
@@ -1,44 +1,46 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define ll long long
-#define ar array
-
-int power(int a, int b) {
-	int res=1;
-	while(b>0) {
-		if(b&1)
-			res*=a;
-		a*=a;
-		b/=2;
+int Power(int a, int b) {
+	int res = 1;
+	while (b > 0) {
+		if (b & 1) {
+			res *= a;
+		}
+		a *= a;
+		b /= 2;
 	}
 	return res;
 }
 
 int main() {
-	ios::sync_with_stdio(0);
+	ios::sync_with_stdio(false);
 	cin.tie(0);
-	
-	//Octal->Decimal->Hexadecimal Process
-	int n;
+	// octal -> decimal -> hexadecimal process
+	long long n;
 	cin >> n;
-	int d=0, rem=0;
-	for(int i=0; n!=0; ++i) {
-		rem=n%10;
-		n/=10;
-		d+=rem*power(8, i);
+	int d = 0;
+	int rem = 0;
+	for (int i = 0; n != 0; i++) {
+		rem = n % 10;
+		n /= 10;
+		d += rem * Power(8, i);
 	}
-	char a[100];
-	int cnt=0;
-	while(d>0) {
-		int tmp=d%16;
-		if(tmp<10)
-			a[cnt++]=48+tmp;
-		else
-			a[cnt++]=55+tmp;
-		d/=16;
+	char a[1000];
+	int cnt = 0;
+	while (d > 0) {
+		int tmp = d % 16;
+		if (tmp < 10) {
+			a[cnt++] = 48 + tmp;
+		} else {
+			a[cnt++] = 55 + tmp;
+		}
+		d /= 16;
 	}
-	for(int i=cnt-1; i>=0; --i)
+	for (int i = cnt - 1; i >= 0; i--) {
 		cout << a[i];
-	cout << "\n";
+	}
+	cout << '\n';
+	return 0;
 }
