c61ee7c4ebb675b1e9463a45580afa6591fceb0b
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-10 18:39:15 +0800

    Update Octal_to_Decimal.cpp

--- Octal_to_Decimal.cpp
+++ Octal_to_Decimal.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
 #define ll long long
-#define ar array;
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
 
 int power(int a, int b) {
 	int res=1;
@@ -19,6 +24,7 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
+	//octal->decimal->binary process
 	int n;
 	cin >> n;
 	int d=0, rem=0;
