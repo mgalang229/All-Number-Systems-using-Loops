7679e0b374633b8b5ea1814de07676303b58d06d
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-10 18:31:02 +0800

    Update Decimal_to_Octal.cpp

--- Decimal_to_Octal.cpp
+++ Decimal_to_Octal.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
 #define ll long long
-#define ar array;
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -11,10 +16,10 @@ int main() {
 	int n;
 	cin >> n;
 	int a[1000], cnt=0;
-	while(n>0) {
+	while(n) {
 		a[cnt++]=n%8;
 		n/=8;
-	}
+	}	
 	for(int i=cnt-1; i>=0; --i)
 		cout << a[i];
 	cout << "\n";
