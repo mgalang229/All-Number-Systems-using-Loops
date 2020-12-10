b51a9437a6dee232dd60ea2f479ce7f4a3e19f27
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-10 18:35:19 +0800

    Update Hexadecimal_to_Decimal.cpp

--- Hexadecimal_to_Decimal.cpp
+++ Hexadecimal_to_Decimal.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
 #define ll long long
-#define ar array;
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
 
 int power(int a, int b) {
 	int res=1;
@@ -18,7 +23,7 @@ int power(int a, int b) {
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
+
 	string s;
 	cin >> s;
 	int b=1, d=0;
