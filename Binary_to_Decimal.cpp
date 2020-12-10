4e50ae6fb6b1c57a95f53454e234167e68a19c4b
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-10 18:12:10 +0800

    Update Binary_to_Decimal.cpp

--- Binary_to_Decimal.cpp
+++ Binary_to_Decimal.cpp
@@ -1,31 +1,36 @@
 #include <bits/stdc++.h>
-
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
-int Power(int a, int b) {
-	int res = 1;
-	while (b > 0) {
-		if (b % 2 == 1) {
-			res *= a;
-		}
-		a *= a;
-		b /= 2;
+#define ll long long
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+int power(int a, int b) {
+	int res=1;
+	while(b>0) {
+		if(b&1)
+			res*=a;
+		a*=a;
+		b/=2;
 	}
 	return res;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
-	long long n;
+	
+	ll n;
 	cin >> n;
-	int d = 0;
-	int rem = 0;
-	for (int i = 0; n != 0; i++) {
-		rem = n % 10;
-		n /= 10;
-		d += rem * Power(2, i);
+	int d=0, rem=0;
+	for(int i=0; n!=0; ++i) {
+		rem=n%10;
+		n/=10;
+		d+=rem*power(2, i);
 	}
-	cout << d << '\n';
-	return 0;
+	cout << d << "\n";
 }
