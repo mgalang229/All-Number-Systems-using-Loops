afbe20630328b10397ab5f7fc98f246d03665382
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-15 20:19:01 +0800

    Update Binary_to_Decimal.cpp

--- Binary_to_Decimal.cpp
+++ Binary_to_Decimal.cpp
@@ -1,8 +1,13 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
 #define ll long long
 #define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
 
 int power(int a, int b) {
 	int res=1;
