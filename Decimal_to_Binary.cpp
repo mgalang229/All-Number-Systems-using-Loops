648929200fcaac5bec284c20281d2e5f54d241c8
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-22 22:58:04 +0800

    Update Decimal_to_Binary.cpp

--- Decimal_to_Binary.cpp
+++ Decimal_to_Binary.cpp
@@ -1,26 +1,19 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
+
 using namespace std;
-using namespace __gnu_pbds;
-
-#define ll long long
-#define ar array
-
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-tree_order_statistics_node_update> indexed_set;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
 	int n;
 	cin >> n;
-	int a[1000], cnt=0;
-	while(n) {
-		a[cnt++]=n%2;
-		n/=2;
+	int a[1000];
+	int cnt = 0;
+	while (n > 0) {
+		a[cnt++] = n % 2;
+		n /= 2;
 	}
-	for(int i=cnt-1; i>=0; --i)
+	for (int i = cnt - 1; i >= 0; i--) {
 		cout << a[i];
-	cout << "\n";
+	}
+	cout << '\n';
+	return 0;
 }
