b7ecd4e8a58b05f48decd9c97dc25af8b6d2c6e4
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-22 22:59:13 +0800

    Update Decimal_to_Hexadecimal.cpp

--- Decimal_to_Hexadecimal.cpp
+++ Decimal_to_Hexadecimal.cpp
@@ -1,31 +1,36 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
+
 using namespace std;
-using namespace __gnu_pbds;
 
-#define ll long long
-#define ar array
-
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-tree_order_statistics_node_update> indexed_set;
+int Power(int a, int b) {
+	int res = 1;
+	while (b > 0) {
+		if (b % 2 == 1) {
+			res *= a;
+		}
+		a *= a;
+		b /= 2;
+	}
+	return res;
+}
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
 	int n;
 	cin >> n;
 	char a[1000];
-	int cnt=0;
-	while(n) {
-		int tmp=n%16;
-		if(tmp<10)
-			a[cnt++]=48+tmp;
-		else
-			a[cnt++]=55+tmp;
-		n/=16;
+	int cnt = 0;
+	while (n > 0) {
+		int tmp = n % 16;
+		if (tmp < 10) {
+			a[cnt++] = 48 + tmp;
+		} else {
+			a[cnt++] = 55 + tmp;
+		}
+		n /= 16;
 	}
-	for(int i=cnt-1; i>=0; --i)
+	for (int i = cnt - 1; i >= 0; i--) {
 		cout << a[i];
-	cout << "\n";
+	}
+	cout << '\n';
+	return 0;
 }
