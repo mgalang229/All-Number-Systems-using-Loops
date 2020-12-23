34eae1102b34741a85f9c6e7cb38a5faa1c5c66a
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-24 00:10:03 +0800

    Update Hexadecimal_to_Octal.cpp

--- Hexadecimal_to_Octal.cpp
+++ Hexadecimal_to_Octal.cpp
@@ -1,32 +1,40 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
+#define ll long long
+#define ar array
+
+int power(int a, int b) {
+	int res=1;
+	while(b) {
+		if(b&1)
+			res*=a;
+		a*=a;
+		b/=2;
+	}
+	return res;
+}
+
 int main() {
-	// hexadecimal to decimal to octal process
+	//hex->decimal->octal process
 	string s;
 	cin >> s;
-	int b = 1;
-	int d = 0;
-	for (int i = (int) s.size() - 1; i >= 0; i--) {
-		if (s[i] >= '0' && s[i] <= '9') {
-			d += (s[i] - 48) * b;
-		} else if (s[i] >= 'A' && s[i] <= 'F') {
-			d += (s[i] - 55) * b;
-		} else if (s[i] >= 'a' && s[i] <= 'f') {
-			d += (s[i] - 87) * b;
-		}
-		b *= 16;
+	int b=1, d=0;
+	for(int i=(int)s.size()-1; i>=0; --i) {
+		if(s[i]>='0'&&s[i]<='9')
+			d+=(s[i]-48)*b;
+		else if(s[i]>='A'&&s[i]<='F')
+			d+=(s[i]-55)*b;
+		else if(s[i]>='a'&&s[i]<='f')
+			d+=(s[i]-87)*b;
+		b*=16;
 	}
-	int a[1000];
-	int cnt = 0;
-	while (d > 0) {
-		a[cnt++] = d % 8;
-		d /= 8;
+	int a[1000], cnt=0;
+	while(d) {
+		a[cnt++]=d%8;
+		d/=8;
 	}
-	for (int i = cnt - 1; i >= 0; i--) {
+	for(int i=cnt-1; i>=0; --i)
 		cout << a[i];
-	}
-	cout << '\n';
-	return 0;
+	cout << "\n";
 }
