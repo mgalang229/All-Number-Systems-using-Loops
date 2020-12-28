2f9b052c9335d08b6c490fd3a3ac9b84f8d9b12b
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-28 17:38:27 +0800

    Update Hexadecimal_to_Decimal.cpp

--- Hexadecimal_to_Decimal.cpp
+++ Hexadecimal_to_Decimal.cpp
@@ -1,21 +1,22 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-#define ll long long
-#define ar array
+using namespace std;
 
 int main() {
 	string s;
 	cin >> s;
-	int b=1, d=0;
-	for(int i=(int)s.size()-1; i>=0; --i) {
-		if(s[i]>='0'&&s[i]<='9')
-			d+=(s[i]-48)*b;
-		else if(s[i]>='A'&&s[i]<='F')
-			d+=(s[i]-55)*b;
-		else if(s[i]>='a'&&s[i]<='f')
-			d+=(s[i]-87)*b;
-		b*=16;
+	int b = 1;
+	int d = 0;
+	for (int i = (int) s.size() - 1; i >= 0; i--) {
+		if (s[i] >= '0' && s[i] <= '9') {
+			d += (s[i] - 48) * b;
+		} else if (s[i] >= 'A' && s[i] <= 'F') {
+			d += (s[i] - 55) * b;
+		} else if (s[i] >= 'a' && s[i] <= 'f') {
+			d += (s[i] - 87) * b;
+		}
+		b *= 16;
 	}
-	cout << d << "\n";
+	cout << d << '\n';
+	return 0;	
 }
