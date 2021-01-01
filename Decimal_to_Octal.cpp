51910031e74d0ee5de945036c21467e22130f907
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-01-01 20:27:22 +0800

    Update Decimal_to_Octal.cpp

--- Decimal_to_Octal.cpp
+++ Decimal_to_Octal.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
+#define ll long long
+#define ar array
+
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
 	int n;
 	cin >> n;
-	int a[1000];
-	int cnt = 0;
-	while (n > 0) {
-		a[cnt++] = n % 8;
-		n /= 8;
+	int a[1000], cnt=0;
+	while(n) {
+		a[cnt++]=n%8;
+		n/=8;
 	}
-	for (int i = cnt - 1; i >= 0; i--) {
+	for(int i=cnt-1; i>=0; --i)
 		cout << a[i];
-	}
-	cout << '\n';
-	return 0;	
+	cout << "\n";
 }
