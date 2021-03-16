689aab30bcc3bb11f87daa9cf8db13fce6ab3a72
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-03-16 20:15:22 +0800

    Update Binary_to_Decimal.cpp

--- Binary_to_Decimal.cpp
+++ Binary_to_Decimal.cpp
@@ -2,23 +2,22 @@
 
 using namespace std;
 
-int Power(int a, int b) {
-	int res = 1;
-	while (b > 0) {
-		if (b % 2 == 1) {
-			res *= a;
-		}
-		a *= a;
-		b /= 2;
-	}
-	return res;
-}
-
 int main() {
 	long long n;
 	cin >> n;
 	int d = 0;
 	int rem = 0;
+	auto Power = [&](int a, int b) {
+		int res = 1;
+		while (b > 0) {
+			if (b & 1) {
+				res *= a;
+			}
+			a *= a;
+			b /= 2;
+		}
+		return res;
+	};
 	for (int i = 0; n != 0; i++) {
 		rem = n % 10;
 		n /= 10;
